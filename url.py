f9beefa02fef922344fb01d3aad74b07d94a22c5
omkarrrrrr <60725732+omkarrrrrr@users.noreply.github.com>
2020-03-16 12:15:21 +0530

    Create url.py

---
+++ url.py
@@ -0,0 +1,9 @@
+import urllib.request
+
+webUrl = urllib.request.urlopen('https://www.youtube.com/user/guru99com'
+                                )
+
+print 'result code: ' + str(webUrl.getcode())
+
+data = webUrl.read()
+print data
